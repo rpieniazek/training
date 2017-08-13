d098ad453a255dbd939cc209068334363dd32720
rafalpieniazek <rkpieniazek@gmail.com>
2017-08-13 21:21:18 +0200

    angular tour of hero - http - get hero by id

--- front/quickstart/src/app/hero/hero.service.ts
+++ front/quickstart/src/app/hero/hero.service.ts
@@ -6,26 +6,28 @@ import {Hero} from "./hero";
 
 @Injectable()
 export class HeroService {
-  private url = 'api/heroes';
+  private apiUrl = 'api/heroes';
 
   constructor(private http: Http) {
 
   }
 
   getHeroes(): Promise<Hero[]> {
-    return this.http.get(this.url)
+    return this.http.get(this.apiUrl)
       .toPromise()
       .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  getHero(id: number): Promise<Hero> {
+    return this.http.get(this.apiUrl + '/' + id)
+      .toPromise()
+      .then(response => response.json().data as Hero)
       .catch(this.handleError);
   }
 
   handleError(error: any) {
     console.error(error);
     return Promise.reject(error.message || error);
-  }
-
-  getHero(id: number): Promise<Hero> {
-    return this.getHeroes()
-      .then(heroes => heroes.find((hero) => hero.id == id));
   }
 }
